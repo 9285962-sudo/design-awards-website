452bbcf93ea72d312146d961470373899c032919
Website Admin <admin@design-awards.com>
2026-04-15 19:38:51 +0800

    Remove old preview folder, use Next.js build

--- fix-git.bat
+++ fix-git.bat
@@ -2,29 +2,14 @@
 chcp 65001 >nul
 cd /d "c:\Users\Administrator\WorkBuddy\国际设计大奖参赛咨询"
 
-if exist ".git" (
-    echo Deleting old .git...
-    rmdir /s /q .git
-)
-
-echo Initializing Git...
-git init
-
-echo Setting user config...
-git config user.email "admin@design-awards.com"
-git config user.name "Website Admin"
-
 echo Adding files...
 git add .
 
 echo Creating commit...
-git commit -m "Initial commit"
-
-echo Adding remote...
-git remote add origin https://github.com/9285962-sudo/design-awards-website.git
+git commit -m "Remove old preview folder, use Next.js build"
 
 echo Pushing...
-git push -u origin master --force
+git push origin master
 
 echo.
 echo Done!
